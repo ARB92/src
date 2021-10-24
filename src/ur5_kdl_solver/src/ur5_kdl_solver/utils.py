3ea235d370413d6f149be105d7f59c05e7d8875a
amarnathbana <amarnath.bj@gmail.com>
2021-10-24 22:13:55 +0200

    wrapping resulted angles to pi

--- src/ur5_kdl_solver/src/ur5_kdl_solver/utils.py
+++ src/ur5_kdl_solver/src/ur5_kdl_solver/utils.py
@@ -1,7 +1,22 @@
 #! /usr/bin/env python
 from geometry_msgs.msg import Pose
+from math import pi
 
 CONSTANT = 10
+
+def wrap_to_pi(angles):
+    wrapped_angles=[]
+    for angle in angles:
+        if angle/abs(angle)==1:
+            angle=angle%(2*pi)
+            if angle>pi:
+                angle=angle-2*pi
+        else:
+            angle=-(abs(angle)%2*pi)
+            if angle<-pi:
+                angle=2*pi+angle
+        wrapped_angles.append(angle)
+    return wrapped_angles
 
 def calculate_new_ee_pose(current_ee_pose,force):
     position = current_ee_pose[0]
